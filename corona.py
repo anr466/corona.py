c13bc54ec8d2e8749973be5aef8574c6493aa63d
eng.abdullah.alruwaili <skaka466@gmail.com>
2020-06-22 23:11:36 +0300

    first commit

---
+++ corona.py
@@ -0,0 +1,106 @@
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import schedule
+import time
+import telebot
+
+
+
+
+# نضع التوكن لبوت التلقرام
+TOKEN = "1041038137:AAHpHkRgWmLQ-08APvNb0Jl-FYtLwe5P_Rw"
+# لازم نضع المعرف الرقمي للقناه بالتلقرام
+chat_id = '174958495'
+#ربط التوكن مع مكتبة التلقرام
+bot = telebot.TeleBot(token=TOKEN)
+#الاوامر
+#updater = Updater(token='TOKEN', use_context=True)
+# الموقع المستخدم لجلب الحالات نضعه في متغير 
+url = 'https://elaph.com/coronavirus-statistics-in-saudi-arabia.html'
+#الاستجابه
+response = requests.get(url)
+#استعراض محتويات الموقع
+page = response.content
+#نستخدم المكتبه BeautifulSoup
+#لا استعراض محتويات الموقع الاندكس بلغة html
+soup = BeautifulSoup(page , 'html.parser')
+
+
+# نبحث عن المحتوى النصي الذي نريد استخدامه او استعراضه من الموقع
+class_for_confirmed_today = soup.find_all(class_='col-lg-6 col-xl-6')
+#هذا الاستعلام يجلب لنا الحالات اليوميه
+today_confirmed_number = [item.find(class_='mb-0 font-30 text-white').get_text() for item in class_for_confirmed_today]
+lable_confirmed_today = [item.find(class_='card-title font-14 text-white').get_text() for item in class_for_confirmed_today]
+
+# يجلب لنا جميع الحالات 
+class_for_all_confirmed = soup.find_all(class_='col-lg-6 col-xl-4')
+all_number_confirmed = [item.find(class_='card-title font-14 text-white').get_text() for item in class_for_all_confirmed]
+lable_all_confirmed = [item.find(class_='mb-0 font-30 text-white').get_text() for item in class_for_all_confirmed]
+
+
+
+
+try:
+    def todayconfirmed():
+        bot.send_message(chat_id,'الحالات اليوميه لمصابي فايروس كورونا بالسعوديه ')
+        index1 = 0 
+        # يعرض اصابات اليوم فقط
+        for item in lable_confirmed_today:
+
+            bot.send_message(chat_id,item)
+            if item == lable_confirmed_today[0]:
+                if today_confirmed_number[0] == 0:
+                     bot.send_message(chat_id,"لم يتم التحديث بعد")
+                else:
+                     bot.send_message(chat_id,today_confirmed_number[0])
+            elif item == lable_confirmed_today[1]:
+                bot.send_message(chat_id,today_confirmed_number[1])
+            elif item == lable_confirmed_today[2]:
+                bot.send_message(chat_id,today_confirmed_number[2])
+            elif item == lable_confirmed_today[3]:
+                bot.send_message(chat_id,today_confirmed_number[3])
+        index1 +=1
+except Exception as error:
+        bot.send_message(chat_id, 'error')
+
+
+
+try:
+    def allconfirmed(): 
+        index2 = 0 
+        bot.send_message(chat_id,text = 'اجمالي عدد الحالات')
+        for item in all_number_confirmed:
+             bot.send_message(chat_id,item)
+             if item == all_number_confirmed[0]:
+                   bot.send_message(chat_id,lable_all_confirmed[0])
+             elif item == all_number_confirmed[1]:
+                 bot.send_message(chat_id,lable_all_confirmed[1])
+             elif item == all_number_confirmed[2]:
+                 bot.send_message(chat_id,lable_all_confirmed[2])
+             elif item == all_number_confirmed[3]:
+                 bot.send_message(chat_id,lable_all_confirmed[3])
+        index2 +=1
+except Exception as er:
+    bot.send_message(chat_id,'error')
+
+
+def showbotdelay():
+
+    todayconfirmed()
+    allconfirmed()
+
+
+
+# البوت يعمل للابد
+while True:
+    try:
+        # يعمل للابد
+        schedule.every().day.at("00:15").do(showbotdelay())
+        bot.polling(none_stop=True)
+        schedule.run_pending()
+
+    except Exception:
+        #هنا في حالة الخروج من البرنامج يتوقف ١٥ ثانيه 
+        time.sleep(15)
