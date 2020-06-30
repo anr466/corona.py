616c5bb6304b71f96405c11893704899663bcb0c
eng.abdullah.alruwaili <skaka466@gmail.com>
2020-06-30 13:12:09 +0300

    edit repo

--- corona.py
+++ corona.py
@@ -75,7 +75,20 @@ def showbotdelay():
 
     return todayconfirmed() ,allconfirmed()
 
+
 showbotdelay()
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, f'الحالات اليوميه  {showbotdelay()}')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+
 
 # البوت يعمل للابد
 while True:
