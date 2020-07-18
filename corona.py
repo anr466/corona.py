f18363fc86499eb3e1d41a2c9e2029f0c90b641c
eng.abdullah.alruwaili <skaka466@gmail.com>
2020-07-18 15:01:38 +0300

    demo-final

--- corona.py
+++ corona.py
@@ -4,6 +4,10 @@ import time ,os
 import flask
 from flask import Flask, request
 import telebot
+from telebot import types
+import json
+
+
 # نضع التوكن لبوت التلقرام
 TOKEN = "1041038137:AAEwfNa6L05P1EqcHGw_JsJ9VF4w6sxsF0o"
 server = Flask(__name__)
@@ -141,17 +145,17 @@ except:
 
 
 @server.route('/' + TOKEN, methods=['POST'])
-#def getMessage():
-	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-	return "!", 200
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
 @server.route("/")
-#def webhook():
-	bot.remove_webhook()
-	bot.set_webhook(url='https://secure-refuge-39491.herokuapp.com/' + TOKEN)
-	return "!", 200
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://secure-refuge-39491.herokuapp.com/' + TOKEN)
+    return "!", 200
 
 
 if __name__ == "__main__":
-	server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
