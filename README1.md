c1960d627b1f3797bd688f5dee86e1984d281c8b
eng.abdullah.alruwaili <skaka466@gmail.com>
2020-07-20 17:55:35 +0300

    final

--- README1.md
+++
@@ -1,3 +0,0 @@
-# corona.py
-
-script for bot telegram retive using bs4 website to covid-19 in saudia arabia
--- corona.py
+++ corona.py
@@ -155,18 +155,3 @@ except:
 
 
 
-#@server.route('/' + TOKEN, methods=['POST'])
-#def getMessage():
- #   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-  #  return "!", 200
-
-
-#@server.route("/")
-#def webhook():
- #   bot.remove_webhook()
-  #  bot.set_webhook(url='https://secure-refuge-39491.herokuapp.com/' + TOKEN)
-   # return "!", 200
-
-
-#if __name__ == "__main__":
- #   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
